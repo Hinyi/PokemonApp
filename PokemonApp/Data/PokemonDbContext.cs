5b99626d240785fe35e9f14791b78494ff29cc87
Hinyi <mrmck16@gmail.com>
2022-12-17 22:13:25 +0100

    Init

---
+++ PokemonApp/Data/PokemonDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PokemonApp.Data
+{
+    public class PokemonDbContext : DbContext
+    {
+        public PokemonDbContext(DbContextOptions<PokemonDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- PokemonApp/Program.cs
+++ PokemonApp/Program.cs
@@ -9,6 +9,7 @@ namespace PokemonApp
             // Add services to the container.
 
 
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
